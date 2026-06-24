4d50ced2981b906d81d65ce477869f453cc5f8af
Yuhi0707 <yuhi07072332@gmail.com>
2026-06-24 23:13:00 +0900

    Add selection page

--- src/pc/pc.js
+++ src/pc/pc.js
@@ -3,7 +3,13 @@ import {RelayServer} from "https://www.chirimen.org/remote-connection/js/beta/Re
 const CHANNEL_NAME = "op2026";
 
 let channel;
-let messageDiv;
+
+// DOM objects
+const messageDiv        = document.getElementById("messageDiv");
+let boardingSelect      = document.getElementById("boardingSelect");     // 乗車駅
+let destinationSelect   = document.getElementById("destinationSelect");  // 降車駅
+let applyButton         = document.getElementById("applyButton");        // 設定通信
+let resetButton         = document.getElementById("resetButton");        // リセット
 
 window.addEventListener("load", main);
 
@@ -14,7 +20,21 @@ async function main(){
 	let relay = RelayServer("chirimentest", "chirimenSocket" );
 	channel = await relay.subscribe(CHANNEL_NAME);
 	messageDiv.innerText="web socketリレーサービスに接続しました";
-	channel.onmessage = msg => messageDiv.innerText = msg.data;
+        channel.onmessage = msg => messageDiv.innerText = msg.data;
+
+    // dom 
+}
+
+/**
+ * 乗車駅/降車駅リストに駅オプションを追加
+ * @param select boardingSelect または destinationSelect
+ * @param name   オプション名前
+*/
+function addStation(select, name) {
+    const option = document.createElement("option");
+    option.value = name;
+    option.textContent = name;
+    select.appendChild(option);
 }
 
 
