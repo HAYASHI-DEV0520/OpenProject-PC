0e4455a19b5e461fc11ca59fcedd47619ffda53a
Yuhi0707 <yuhi07072332@gmail.com>
2026-06-25 00:00:15 +0900

    Add apply button and reset button

--- src/pc/pc.js
+++ src/pc/pc.js
@@ -5,36 +5,59 @@ const CHANNEL_NAME = "op2026";
 let channel;
 
 // DOM objects
-const messageDiv        = document.getElementById("messageDiv");
-let boardingSelect      = document.getElementById("boardingSelect");     // 乗車駅
-let destinationSelect   = document.getElementById("destinationSelect");  // 降車駅
-let applyButton         = document.getElementById("applyButton");        // 設定通信
-let resetButton         = document.getElementById("resetButton");        // リセット
+const messageDiv          = document.getElementById("messageDiv");
+const lineSelect          = document.getElementById("lineSelect");         // 線路
+const boardingSelect      = document.getElementById("boardingSelect");     // 乗車駅
+const destinationSelect   = document.getElementById("destinationSelect");  // 降車駅
+const applyButton         = document.getElementById("applyButton");        // 設定通信
+const resetButton         = document.getElementById("resetButton");        // リセット
+const selectedBoarding    = document.getElementById("selectedBoarding");
+const selectedDestination = document.getElementById("selectedDestination");
 
 window.addEventListener("load", main);
 
 async function main(){
-    messageDiv =  document.getElementById("messageDiv")
-
 	// webSocketリレーの初期化
 	let relay = RelayServer("chirimentest", "chirimenSocket" );
 	channel = await relay.subscribe(CHANNEL_NAME);
 	messageDiv.innerText="web socketリレーサービスに接続しました";
         channel.onmessage = msg => messageDiv.innerText = msg.data;
 
-    // dom 
+    applyButton.addEventListener(
+        "click",
+        () => {
+            selectedBoarding.textContent = boardingSelect.value;
+            selectedDestination.textContent = destinationSelect.value;
+        }
+    )
+    resetButton.addEventListener(
+        "click",
+        () => {
+            selectedBoarding.textContent = "未選択";
+            selectedDestination.textContent = "未選択";
+        }
+    )
+
+    // テスト用
+    const oedoLineOption = document.createElement("option");
+    oedoLineOption.value = "大江戸線";
+    oedoLineOption.textContent = "大江戸線";
+    lineSelect.appendChild(oedoLineOption);
+    addStation("春日駅");
+    addStation("飯田橋駅");
+    addStation("門前仲町駅");
+
 }
+
+
 
 /**
  * 乗車駅/降車駅リストに駅オプションを追加
- * @param select boardingSelect または destinationSelect
- * @param name   オプション名前
 */
-function addStation(select, name) {
+function addStation(name) {
     const option = document.createElement("option");
     option.value = name;
     option.textContent = name;
-    select.appendChild(option);
+    boardingSelect.appendChild(option);
+    destinationSelect.appendChild(option.cloneNode(true));
 }
-
-
